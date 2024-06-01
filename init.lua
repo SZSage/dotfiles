99fc37d6a0f7f70dfcb902795f29540086dafac5
SZSage <simonz@uoregon.edu>
2024-06-01 16:06:34 -0700

    Moved some configs around

--- init.lua
+++ init.lua
@@ -1,7 +1,7 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 require("config.autocmds")
-require('mini.animate').setup({
+require("mini.animate").setup({
   open = { enable = false },
   close = { enable = false },
 })
@@ -10,6 +10,41 @@ require("notify").setup({
   background_colour = "#000000",
 })
 
+-- LSP configurations
+local lspconfig = require('lspconfig')
+local util = require('lspconfig/util')
+local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+-- Utility function to find the root directory
+local function get_root_dir(fname)
+  return vim.fn.getcwd()
+end
+
+-- Configure pyright
+lspconfig.pyright.setup{
+  capabilities = capabilities,
+  cmd = {"/Users/sage/.local/share/nvim/mason/bin/pyright-langserver", "--stdio"},
+  filetypes = {"python"},
+  root_dir = get_root_dir,
+  settings = {
+    python = {
+      analysis = {
+        autoSearchPaths = true,
+        useLibraryCodeForTypes = true,
+        diagnosticMode = "workspace",
+        typeCheckingMode = "basic",
+        logLevel = "Information",
+      }
+    }
+  }
+}
+
+-- Configure ruff_lsp
+lspconfig.ruff_lsp.setup{
+  cmd = {"/Users/sage/.local/share/nvim/mason/bin/ruff-lsp"},
+  filetypes = {"python"},
+  root_dir = get_root_dir,
+}
 
 -- setting for automatically change colorscheme based on light/dark mode from system
 --[[
@@ -30,49 +65,50 @@ require('auto-dark-mode').setup({
 })
 ]]
 
-
-
 -- setup must be called before loading
-require('lualine').setup( {
+require("lualine").setup({
   options = {
-    theme = 'nightfox'
-  }
+    theme = "nightfox",
+  },
 })
 
 -- [[ Configure Treesitter ]]
 -- see `:help nvim-treesitter`
-require("nvim-treesitter.configs").setup {
-	-- add languages to be installed here that you want installed for treesitter
-	ensure_installed = {"sql"},
-	highlight = { enable = true },
-	indent =  { enable = true},
-	incremental_selection = {
-		enable = true,
-	}
+require("nvim-treesitter.configs").setup({
+  -- add languages to be installed here that you want installed for treesitter
+  ensure_installed = { "sql" },
+  highlight = { enable = true },
+  indent = { enable = true },
+  incremental_selection = {
+    enable = true,
+  },
+})
 
-}
-
-local harpoon = require('harpoon')
+local harpoon = require("harpoon")
 harpoon:setup({})
 
 -- basic telescope configuration
 local conf = require("telescope.config").values
 local function toggle_telescope(harpoon_files)
-    local file_paths = {}
-    for _, item in ipairs(harpoon_files.items) do
-        table.insert(file_paths, item.value)
-    end
+  local file_paths = {}
+  for _, item in ipairs(harpoon_files.items) do
+    table.insert(file_paths, item.value)
+  end
 
-    require("telescope.pickers").new({}, {
-        prompt_title = "Harpoon",
-        finder = require("telescope.finders").new_table({
-            results = file_paths,
-        }),
-        previewer = conf.file_previewer({}),
-        sorter = conf.generic_sorter({}),
-    }):find()
+  require("telescope.pickers")
+    .new({}, {
+      prompt_title = "Harpoon",
+      finder = require("telescope.finders").new_table({
+        results = file_paths,
+      }),
+      previewer = conf.file_previewer({}),
+      sorter = conf.generic_sorter({}),
+    })
+    :find()
 end
 
-vim.keymap.set("n", "<C-e>", function() toggle_telescope(harpoon:list()) end,
-    { desc = "Open harpoon window" })
 
+
+vim.keymap.set("n", "<C-e>", function()
+  toggle_telescope(harpoon:list())
+end, { desc = "Open harpoon window" })
