5e941fe3d03ac587786f8c79be182862757b0d62
SZSage <simonz@uoregon.edu>
2024-06-14 18:46:23 -0700

    nvim autocompletion

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,31 +1,86 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
--- Add any additional autocmds here
-
+-- Colorscheme setup
+vim.cmd [[colorscheme nightfox]]
 vim.cmd [[
   highlight NormalFloat guibg=NONE
   highlight FloatBorder guibg=NONE
   highlight NvimTreeNormal guibg=NONE
   highlight LazyTerm guibg=NONE
 ]]
--- setup must be called before loading
-vim.cmd("colorscheme nightfox")
--- fixes colorscheme not matching
 vim.opt.termguicolors = true
 
+-- Lualine setup
 require('lualine').setup({
-	{
-		options = {
-			theme = "nightfox"
-		}
-	}
+  options = {
+    theme = "nightfox"
+  }
 })
 
--- config/autocmds.lua
+-- Setup for nvim-cmp and autocompletion
+vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+local cmp = require('cmp')
+cmp.setup({
+  snippet = {
+    expand = function(args)
+      require('luasnip').lsp_expand(args.body)
+    end,
+  },
+  window = {
+    completion = {
+      border = "rounded",
+      winhighlight = "Normal:Pmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+    },
+    documentation = {
+      border = "rounded",
+      winhighlight = "Normal:Pmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
+    },
+  },
+  mapping = cmp.mapping.preset.insert({
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+    ['<C-Space>'] = cmp.mapping.complete(),
+    ['<C-e>'] = cmp.mapping.abort(),
+    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+  }),
+  sources = cmp.config.sources({
+    { name = 'nvim_lsp' },
+    { name = 'nvim_lua' },
+    { name = 'luasnip' },
+  }, {
+    { name = 'buffer' },
+    { name = 'path' },
+  }),
+})
+
+-- Setup for nvim-cmp in command line mode
+cmp.setup.cmdline(':', {
+  mapping = cmp.mapping.preset.cmdline({
+    ['<Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_next_item()
+      else
+        fallback()
+      end
+    end,
+    ['<S-Tab>'] = function(fallback)
+      if cmp.visible() then
+        cmp.select_prev_item()
+      else
+        fallback()
+      end
+    end,
+  }),
+  sources = cmp.config.sources({
+    { name = 'path' },
+  }, {
+    { name = 'cmdline' },
+  }),
+})
+
+-- Autocmds for specific filetypes
 local autocomplete_group = vim.api.nvim_create_augroup("vimrc_autocompletion", { clear = true })
-local cmp = require("cmp")
 vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "sql", "mysql", "plsql" },
+  pattern = { "sql", "mysql", "plsql", "python" },
   callback = function()
     cmp.setup.buffer({
       sources = {
@@ -38,24 +93,22 @@ vim.api.nvim_create_autocmd("FileType", {
   group = autocomplete_group,
 })
 
-
--- Disable autoformat for lua files
-vim.api.nvim_create_autocmd({ "FileType" }, {
+-- Disable autoformat for C files
+vim.api.nvim_create_autocmd("FileType", {
   pattern = { "c" },
   callback = function()
     vim.b.autoformat = false
   end,
 })
 
--- turn off paste mode when leaving insert
+-- Turn off paste mode when leaving insert mode
 vim.api.nvim_create_autocmd("InsertLeave", {
-	pattern = "*",
-	command = "set nopaste"
+  pattern = "*",
+  command = "set nopaste"
 })
 
--- displays a small diagnostic window
+-- Display diagnostic window on cursor hold
 vim.api.nvim_create_autocmd("CursorHold", {
-  buffer = bufnr,
   callback = function()
     local opts = {
       focusable = false,
@@ -64,70 +117,23 @@ vim.api.nvim_create_autocmd("CursorHold", {
       source = 'always',
       prefix = ' ',
       scope = 'cursor',
-	  winhl = "Normal:NormalNC",
+      winhl = "Normal:NormalNC",
     }
     vim.diagnostic.open_float(nil, opts)
   end
 })
 
+-- Custom highlight
 vim.cmd("highlight CustomBorder guibg=NONE")
-local cmp = require("cmp")
-vim.opt.completeopt = { "menu", "menuone", "noselect" }
-cmp.setup({
-    snippet = {
-        expand = function(args)
-            require("luasnip").lsp_expand(args.body)
-        end,
-    },
 
-    window = {
-        completion = {
-            border = "rounded",
-            winhighlight = "Normal:Pmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
-        },
-        documentation = {
-            border = "rounded",
-            winhighlight = "Normal:Pmenu,FloatBorder:CmpPmenuBorder,CursorLine:PmenuSel,Search:None",
-        },
-    },
- 
-    mapping = cmp.mapping.preset.insert({
-        ["<C-b>"] = cmp.mapping.scroll_docs(-4),
-        ["<C-f>"] = cmp.mapping.scroll_docs(4),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-e>"] = cmp.mapping.abort(),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-    }),
-    sources = cmp.config.sources({
-        { name = "nvim_lsp" },
-        { name = "nvim_lua" },
-        { name = "luasnip" },
-    }, {
-        { name = "buffer" },
-        { name = "path" },
-    }),
-})
-cmp.setup.cmdline(":", {
-    mapping = cmp.mapping.preset.cmdline({
-        ["<Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_next_item()
-            else
-                fallback()
-            end
-        end,
-        ["<S-Tab>"] = function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            else
-                fallback()
-            end
-        end,
-	}),
-    sources = cmp.config.sources({
-        { name = "path" },
-    }, {
-        { name = "cmdline" },
-    }),
-})
+-- Ensure pretty_hover is correctly configured
+require('pretty_hover').setup{}
 
+-- Bind pretty_hover to <Leader>K
+vim.api.nvim_set_keymap('n', '<Leader>K', '<cmd>lua require("pretty_hover").hover()<CR>', { noremap = true, silent = true })
+
+-- Register the key mapping with which-key
+local wk = require("which-key")
+wk.register({
+  ["<Leader>K"] = { "<cmd>lua require('pretty_hover').hover()<CR>", "Pretty Hover" }
+})
