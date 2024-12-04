e73c9ef30e317ee5a745114bce10fb13d022b1c2
SZSage <simonz@uoregon.edu>
2024-12-04 14:13:51 -0800

    disabled some plugins

--- nvim/lua/plugins/disabled.lua
+++ nvim/lua/plugins/disabled.lua
@@ -1,7 +1,12 @@
 return {
-  { "folke/tokyonight.nvim", enabled = false },
+  { "folke/tokyonight.nvim", enabled = true },
   { "NeogitOrg/neogit", enabled = false },
+  { "echasnovski/mini.animate", enabled = false },
   { "echasnovski/mini.ai", enabled = false },
   { "numToStr/Comment.nvim", enabled = false },
-  { "rafamadriz/friendly-snippets"},
+  { "p00f/clangd_extensions.nvim", enabled = false },
+  { "rafamadriz/friendly-snippets", enabled = false },
+  { "ptzz/lf.vim", enabled = false },
+  { "jbyuki/one-small-step-for-vimkind", enabled = false },
+  { "tree-sitter-grammars/tree-sitter-markdown", enabled = false },
 }
